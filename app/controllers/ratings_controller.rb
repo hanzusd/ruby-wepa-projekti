bfae3fc0f5c0ccdded7cb99de04272c2dc25fe81
hanzusd <hannalotta.alenius@helsinki.fi>
2018-09-16 16:40:43 +0300

    pluralizing

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,10 +1,18 @@
 class RatingsController < ApplicationController
-    before_action :set_rating, only: [:show, :edit, :update, :destroy]
-
+    
   # GET /ratings
   # GET /ratings.json
   def index
     @ratings = Rating.all
+  end
+
+  def new
+    @rating = Rating.new
+  end
+
+  def create
+    Rating.create params.require(:rating).permit(:score, :beer_id)
+    redirect_to ratings_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
 
   get 'ratings', to: 'ratings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'ratings/new', to:'ratings#new'
+  post 'ratings', to: 'ratings#create'
+
 end
