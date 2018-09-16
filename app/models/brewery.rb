45e4ce8e6da0a5139eaba916ae6deaf89043d6fb
hanzusd <hannalotta.alenius@helsinki.fi>
2018-09-16 18:41:57 +0300

    brewery ratings

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,11 @@
 class Brewery < ApplicationRecord
     has_many :beers, dependent: :destroy
+    has_many :ratings, through: :beers
+
+    def average_rating
+        sum = 0.0;
+        ratings.each{ |rating| sum += rating.score}
+        return sum/(ratings.count)
+    end
+    
 end
