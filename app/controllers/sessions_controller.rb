037d798d8403332e766685c311f0e17aec9de634
hanzusd <hannalotta.alenius@helsinki.fi>
2018-09-27 22:28:30 +0300

    signin test error fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_path(user), notice: "Welcome back!"
     else
-      redirect_to :back, notice: "Username and/or password mismatch"
+      redirect_to signin_path, notice: "Username and/or password mismatch"
     end
   end
 
