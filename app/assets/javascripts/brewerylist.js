2b0fd14e6ff87c6bf6b58a24706fca8da92afb57
hanzusd <hannalotta.alenius@helsinki.fi>
2018-10-22 10:41:00 +0300

    sorting breweries

--- app/assets/javascripts/brewerylist.js
+++ app/assets/javascripts/brewerylist.js
@@ -1,6 +1,7 @@
 const BREWERIES = {}
 
 BREWERIES.show = () => {
+  $("#brewerytable tr:gt(0)").remove()
   const table = $("#brewerytable")
 
   BREWERIES.list.forEach((brewery) => {
@@ -12,8 +13,45 @@ BREWERIES.show = () => {
       + '</tr>')
   })
 }
+BREWERIES.sort_by_name = () => {
+    BREWERIES.list.sort((a, b) => {
+      return a.name.toUpperCase().localeCompare(b.name.toUpperCase());
+    })
+  }
+  
+  BREWERIES.sort_by_year = () => {
+    BREWERIES.list.sort((a, b) => {
+      return a >= b
+    })
+  }
 
-document.addEventListener("turbolinks:load", () => {
+  BREWERIES.sort_by_beers = () => {
+      BREWERIES.list.sort((a, b) => {
+          return a >= b
+      })
+  }
+
+  
+  document.addEventListener("turbolinks:load", () => {
+    $("#name").click((e) => {
+      e.preventDefault()
+      BREWERIES.sort_by_name()
+      BREWERIES.show();
+      
+    })
+  
+    $("#year").click((e) => {
+      e.preventDefault()
+      BREWERIES.sort_by_year()
+      BREWERIES.show()
+    })
+
+    $("#beers").click((e) => {
+        e.preventDefault()
+        BREWERIES.sort_by_year()
+        BREWERIES.show()
+      })
+  
     $.getJSON('breweries.json', (breweries) => {
       BREWERIES.list = breweries
       BREWERIES.show()
